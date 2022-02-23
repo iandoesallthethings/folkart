3bd2a91dfe3cbc6ff71983a1626e95b5e07b059d
iandoesallthethings <denkyuu@gmail.com>
2022-02-22 21:02:45 -0500

    Implement autoplay.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,10 +10,28 @@
 <script lang="ts">
 	export let recordings = []
 
-	let currentRecording
+	let current = 0
+	let autoplay = true
 
-	function load(recording) {
-		currentRecording = recording
+	function wrap(num: number, min: number, max: number): number {
+		return num >= min ? num % max : ((num % max) + max) % max
+	}
+
+	function load(recordingIndex) {
+		current = recordingIndex
+	}
+
+	function previous() {
+		current = wrap(current - 1, 0, recordings.length)
+	}
+
+	function next() {
+		current = wrap(current + 1, 0, recordings.length)
+	}
+
+	function autoNext() {
+		if (!autoplay) return
+		next()
 	}
 </script>
 
@@ -22,20 +40,28 @@
 <h2>Recordings</h2>
 
 <section class="player">
-	{#key currentRecording}
-		<h3>{currentRecording?.Name || ' '}</h3>
-		<audio controls>
-			<source src={currentRecording?.Latest} type="audio/mpeg" />
-		</audio>
+	{#key current}
+		<div>
+			<h3>{recordings[current].Name || ' '}</h3>
+			<audio controls on:ended={autoNext} {autoplay}>
+				<source src={recordings[current].Latest} type="audio/mpeg" />
+			</audio>
+		</div>
 	{/key}
+
+	<div>
+		<button on:click={previous}>⏮</button>
+		<button on:click={next}>⏭</button>
+		<label>
+			<input type="checkbox" bind:checked={autoplay} />
+			Autoplay
+		</label>
+	</div>
 </section>
 
 <section class="list">
-	{#each recordings as recording}
-		<div
-			class="list-card {currentRecording?.id === recording?.id ? 'selected' : ''}"
-			on:click={() => load(recording)}
-		>
+	{#each recordings as recording, i}
+		<div class="list-card {current === i ? 'selected' : ''}" on:click={() => load(i)}>
 			<div>{recording.Name}</div>
 			<!-- <div>{recordings.Written}</div> -->
 		</div>
@@ -48,7 +74,7 @@
 	}
 
 	.player {
-		@apply border rounded-md p-2;
+		@apply border rounded-md p-2 flex items-center justify-between;
 	}
 
 	.list {
