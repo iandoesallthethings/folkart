d97f8a312347564df6fcf1e77d30b495e555c3e3
Alexander Eversbusch <alexandereversbusch@AlexandersMBP2.attlocal.net>
2023-03-16 19:14:15 -0400

    Add a height rule to the section containing the song list.

--- src/lib/TrackList.svelte
+++ src/lib/TrackList.svelte
@@ -12,22 +12,22 @@
 
 <section class="list">
 	{#each recordings as recording, i}
-		<div class="list-card {current === i ? 'selected' : ''}" on:click={() => select(i)}>
-			<div>{recording.Name}</div>
-		</div>
+	<div class="list-card {current === i ? 'selected' : ''}" on:click={()=> select(i)}>
+		<div>{recording.Name}</div>
+	</div>
 	{/each}
 </section>
 
 <style>
 	.list {
-		@apply divide-y border rounded-md overflow-y-scroll w-full;
+		@apply divide-y border rounded-md overflow-y-scroll h-1 w-full;
 	}
 
 	.list-card {
-		@apply p-2 cursor-pointer flex justify-between hover:bg-sky-100 transition;
+		@apply p-2 cursor-pointer flex justify-between hover: bg-sky-100 transition;
 	}
 
 	.list-card.selected {
-		@apply bg-sky-300 hover:bg-sky-200 dark:text-gray-800 transition;
+		@apply bg-sky-300 hover: bg-sky-200 dark:text-gray-800 transition;
 	}
 </style>
