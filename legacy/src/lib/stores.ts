30027de9e244945fc2329007493860ce97ac3cc6
iandoesallthethings <iandoesallthethings@gmail.com>
2023-03-20 01:36:57 -0400

    Ditch legacy folder.

--- legacy/src/lib/stores.ts
+++
@@ -1,7 +0,0 @@
-import localStore from '$lib/localStore'
-
-const autoplay = localStore('autoplay', true)
-const darkMode = localStore('dark-mode', false)
-const showZoomView = localStore('show-zoom-view', false)
-
-export { autoplay, darkMode, showZoomView }
